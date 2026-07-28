0da3ed92bfbc3779afef260b841611945c5ca9d1
emrezeyrek <yemrezeyrek.1@gmail.com>
2026-07-28 14:01:01 +0300

    Task 4: yellow rectangle in taskimage

---
+++ task6/main.py
@@ -0,0 +1,17 @@
+import cv2
+
+img = cv2.imread("leaf.png")
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+ret, thresholded = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+
+
+cv2.imshow("original", img)
+cv2.imshow("gray", gray)
+cv2.imshow("thresholded", thresholded)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imwrite("thresholded_leaf.png", thresholded)
+
