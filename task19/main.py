93a263951e476dae07abdf82466a19dc19115e83
emrezeyrek <yemrezeyrek.1@gmail.com>
2026-08-04 09:50:21 +0300

    Task19

---
+++ task19/main.py
@@ -0,0 +1,73 @@
+import time
+
+import cv2
+import numpy as np
+
+net = cv2.dnn.readNetFromDarknet("yolov4.cfg", "yolov4.weights")
+
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+
+with open("coco.names", "r") as f:
+    classes = f.read().strip().split("\n")
+
+capture = cv2.VideoCapture(1)
+
+if not capture.isOpened():
+    print("VideoWriter didnt open correctly")
+    exit()
+
+while capture.isOpened():
+    start_time = time.time()
+    boxes = []
+    confidences = []
+
+    ret, frame = capture.read()
+
+    if not ret:
+        print("Cant receive frame. exiting..")
+        break
+
+    frame = cv2.flip(frame, 1)
+    blob = cv2.dnn.blobFromImage(frame, 1/255.0, (416, 416), swapRB=True, crop=False)
+    net.setInput(blob)
+    outputs = net.forward(output_layers)
+
+    height, width = frame.shape[:2]
+
+    for out in outputs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+
+            if classes[class_id] == "person" and confidence > 0.5:
+                confidences.append(float(confidence))
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+                boxes.append([x, y, w, h])
+
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+    for i in indices:
+        x, y, w, h = boxes[i]
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.putText(frame, "Person", (x, y - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+
+    end_time = time.time()
+    fps = 1 / (end_time - start_time)
+    cv2.putText(frame, f"FPS: {round(fps, 1)}", (width - 200, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 0), 4)
+
+
+    cv2.imshow("frame", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+capture.release()
